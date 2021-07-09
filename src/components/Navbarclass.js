3ee7e61b2db3dcdcf96e83b97f8383b3ce811475
wafachaari <wafachaari30@gmail.com>
2021-07-09 16:05:16 +0200

    updated

--- src/components/Navbarclass.js
+++ src/components/Navbarclass.js
@@ -1,9 +1,9 @@
- 
+
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from 'react-bootstrap';
 import icon from '../img/logo-header.svg'
-import {  
+import {
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
@@ -11,29 +11,30 @@ import {
 function Navbarclass() {
   return (
     <div>
-    <Navbar collapseOnSelect expand="lg" >
-      <Navbar.Brand href="#home" > <img src={icon}  className="img-fluid logo" alt="brand" /></Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto"  defaultActiveKey="#home" >         
-          <Nav.Link className="navlink" as={Link} to="/final">    <AiOutlineHome style={{ marginBottom: "2px" }} /> 
-            Home
-          </Nav.Link>
-          <Nav.Link className="navlink" as={Link} to="/About">
-          <AiOutlineUser style={{ marginBottom: "2px" } } />
-            About
-          </Nav.Link>
-          <Nav.Link className="navlink"  as={Link} to="/Projects">
-        
-          <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-            projects
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-    <div className="ligne" ></div>
+      <Navbar collapseOnSelect expand="lg" >
+        <Navbar.Brand href="#home" > <img src={icon} className="img-fluid logo" alt="brand" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse  id="responsive-navbar-nav">
+          <Nav my-2 my-lg-0 className="ml-auto" defaultActiveKey="#home" >
+            <Nav.Link className="navlink" as={Link} to="/">
+              <AiOutlineHome style={{ marginBottom: "2px" }} />
+              Home
+            </Nav.Link>
+            <Nav.Link className="navlink" as={Link} to="/About">
+              <AiOutlineUser style={{ marginBottom: "2px" }} />
+              About
+            </Nav.Link>
+            <Nav.Link className="navlink" as={Link} to="/Projects">
+
+              <AiOutlineFundProjectionScreen
+                style={{ marginBottom: "2px" }}
+              />{" "}
+              projects
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="ligne" ></div>
     </div>
 
   )
